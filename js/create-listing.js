985387416d36bb0d079779a3ce37902f73570ca1
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-23 19:40:41 +0100

    work on create entry

--- js/create-listing.js
+++ js/create-listing.js
@@ -24,25 +24,35 @@ const baseUrlCE = "https://api.noroff.dev/api/v1"
 
 newListBtn.addEventListener("click", createEntry);
 
+const myTag = "Cat doge bird"
+const myTagArray = myTag.split(" ")
+console.log(myTagArray)
+
+
 async function createEntry () {
     const ceTags = document.querySelector(".createEntryTags").value;
+    const ceTagsArray = ceTags.split(" ")
     const ceMedia = document.querySelector(".createEntryMedia").value;
+    const ceMediaArray = ceMedia.split(" ")
     const ceEndDate = document.querySelector(".createEntryEndDate").value;
     const ceDesc = document.querySelector(".createEntryDesc").value;
     const ceTitle = document.querySelector(".createEntryTitle").value;
     const entry = {
         title: ceTitle,
         description: ceDesc,
-        tags: ceTags,
-        media: ceMedia,
+        tags: ceTagsArray,
+        media: ceMediaArray,
         endsAt: ceEndDate,
     };
+
     console.log(entry)
     try{
         const reply = await fetch(`${baseUrlCE}/auction/listings`, {
             method: "POST",
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+
             },
             body: JSON.stringify(entry),
         });
