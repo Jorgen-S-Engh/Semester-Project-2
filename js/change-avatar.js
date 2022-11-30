cf582a533ff9e97e1a9a04bc6683264c870a462e
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-30 22:46:39 +0100

    finish change media

--- js/change-avatar.js
+++ js/change-avatar.js
@@ -2,16 +2,19 @@ import { baseUrl } from "./components/baseUrl.mjs";
 import { setImg } from "./components/setProfileImg.mjs";
 
 const btnChangeAvatar = document.querySelector(".btn-change-avatar");
-const inputMedia = document.querySelector(".CA-media").value;
+const inputMedia = document.querySelector(".CA-media");
+const profileImg = document.querySelector(".profile-user-image");
 
 btnChangeAvatar.addEventListener("click", changeMedia);
 
 async function changeMedia (e){
     e.preventDefault();
+    
 
     const avatar = {
-        avatar: inputMedia,
+        avatar: inputMedia.value,
     };
+    console.log(inputMedia);
 
     try{
         const response = await fetch(`${baseUrl}/auction/profiles/${localStorage.getItem("name")}/media`, {
@@ -24,15 +27,18 @@ async function changeMedia (e){
             body: JSON.stringify(avatar)
         })
         const data = await response.json();
-        
-        localStorage.setItem("avatar", "");
-        setImg();
-        
-        location.reload();
         console.log(response)
         console.log(data)
 
     }catch(e){
         console.log(e)
+    }
+    finally{
+        // localStorage.removeItem('avatar');
+        localStorage.setItem("avatar", inputMedia.value);
+        profileImg.src = inputMedia.value;
+        inputMedia.value = "";
+        location.reload();
+
     }
 }
--- js/components/setProfileImg.mjs
+++ js/components/setProfileImg.mjs
@@ -6,5 +6,4 @@ export function setImg () {
         profileImg.src = "img/user-profile.png"
     } 
 }
-
 setImg();
